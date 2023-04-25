ccb1f0f170f11745259a678038fb2f597ddcc58e
Ankit <root@ip-172-31-41-78.ap-south-1.compute.internal>
2023-04-25 10:43:08 +0000

    changing code

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!....edited on 19thfeb2023 (Modified v0.1')
+        'body': json.dumps('Hello from Lambda!....edited on 19thfeb2023 (Publicis')
     }
